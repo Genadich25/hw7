4c9806e06ad842d383f3bbd7a8969158a47dddca
pin20 <pin200048@gmail.com>
2022-01-13 20:16:36 +0300

    Инициализация

--- src/ru/lookBag/Main.java
+++ src/ru/lookBag/Main.java
@@ -3,6 +3,25 @@ package ru.lookBag;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //1 задача
+
+        System.out.println();
+
+        //2 задача
+
+        System.out.println();
+
+        //3 задача
+
+        System.out.println();
+
+        //4 задача
+
+        System.out.println();
+
+        //5 задача
+
+        System.out.println();
+
     }
 }
